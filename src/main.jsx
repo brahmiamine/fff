b3f29db6fc2d3274ffb104b02f33cde8bb64b3fb
brahmiamine <ybrahmi66@gmail.com>
2026-08-25 21:49:47 +0200

    style: load memorized question styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css'
 import './styles/learning.css'
+import './styles/memorized.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
