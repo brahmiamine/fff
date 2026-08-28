992b3dc10a5d343a3f52f50b0f9994d024f01283
brahmiamine <ybrahmi66@gmail.com>
2026-08-28 12:04:03 +0200

    Supprime la configuration manuelle des lots

--- src/data/questionLots.js
+++
@@ -1,6 +0,0 @@
-export const QUESTION_LOTS = Object.freeze([
-  { id: 'lot1', label: 'Lot 1' },
-  { id: 'lot2', label: 'Lot 2' },
-])
-
-export const QUESTION_LOT_IDS = Object.freeze(QUESTION_LOTS.map((lot) => lot.id))
